e9e9b27c0c7016bf160a2fc1e4d7f77cb32d0388
jamesdunaway95 <79390069+jamesdunaway95@users.noreply.github.com>
2021-07-01 19:20:00 -0700

    Uploaded completed Lab 4 cpp file

--- Lab4-2/VectorSorting.cpp
+++ Lab4-2/VectorSorting.cpp
@@ -1,12 +1,14 @@
 //============================================================================
 // Name        : VectorSorting.cpp
-// Author      : Your name
+// Author      : James Dunaway
+// Course      : CS-260-T5434 Data Structures and Algorithms
 // Version     : 1.0
 // Copyright   : Copyright © 2017 SNHU COCE
 // Description : Vector Sorting Algorithms
 //============================================================================
 
 #include <algorithm>
+#include <vector>
 #include <iostream>
 #include <time.h>
 
@@ -121,7 +123,42 @@ vector<Bid> loadBids(string csvPath) {
  * @param end Ending index to partition
  */
 int partition(vector<Bid>& bids, int begin, int end) {
+	int low = begin;
+	int high = end;
+	Bid tempBid;
 
+	// Pick the middle as the pivot
+	int pivot = begin + (end - begin)/2;
+
+	bool done = false;
+
+	while (!done) {
+
+		// continuously increment low point until it is greater than pivot
+		while (bids.at(low).title.compare(bids.at(pivot).title) < 0) {
+			++low;
+		}
+
+		// Continuously decrement high point until it is less than pivot
+		while (bids.at(pivot).title.compare(bids.at(high).title) < 0) {
+			--high;
+		}
+
+		if (low >= high) {
+			done = true;
+		} else {
+			// Swap bid position with smallest bid using temp bid var
+			tempBid = bids[low];
+			bids[low] = bids[high];
+			bids[high] = tempBid;
+
+			// Move low/high points
+			++low;
+			--high;
+		}
+	}
+
+	return high;
 }
 
 /**
@@ -134,6 +171,19 @@ int partition(vector<Bid>& bids, int begin, int end) {
  * @param end the ending index to sort on
  */
 void quickSort(vector<Bid>& bids, int begin, int end) {
+	int mid = 0;
+
+	// Stop the recursive function if we are out of bids to sort
+	if (begin >= end) {
+		return;
+	}
+
+	// Partition bids
+	mid = partition(bids, begin, end);
+
+	// Recursive calls
+	quickSort(bids, begin, mid);
+	quickSort(bids, mid + 1, end);
 }
 
 // FIXME (1a): Implement the selection sort logic over bid.title
@@ -147,6 +197,26 @@ void quickSort(vector<Bid>& bids, int begin, int end) {
  *            instance to be sorted
  */
 void selectionSort(vector<Bid>& bids) {
+	int min;
+	Bid tempBid;
+
+	// outer for loop updates bid vector
+	for (unsigned int i = 0; i < bids.size() - 1; ++i) {
+		min = i;
+
+		// Inner for loop compares vector positions and updates min var
+		for (unsigned int j = i + 1; j < bids.size(); ++j) {
+			if (bids.at(j).title.compare(bids.at(min).title) < 0) {
+				min = j;
+
+			}
+		}
+
+		// Swap bid position with smallest bid using temp bid var
+		tempBid = bids[i];
+		bids[i] = bids[min];
+		bids[min] = tempBid;
+	}
 }
 
 /**
@@ -222,9 +292,38 @@ int main(int argc, char* argv[]) {
             break;
 
         // FIXME (1b): Invoke the selection sort and report timing results
+        case 3:
+            // Initialize a timer variable before loading bids
+            ticks = clock();
+
+            // Complete the method call sort the bids
+            selectionSort(bids);
+
+            cout << bids.size() << " bids read" << endl;
+
+            // Calculate elapsed time and display result
+            ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+            cout << "time: " << ticks << " clock ticks" << endl;
+            cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+
+            break;
 
         // FIXME (2b): Invoke the quick sort and report timing results
+        case 4:
+			   // Initialize a timer variable before loading bids
+			   ticks = clock();
 
+			   // Complete the method call quicksort the bids using the first and last elements
+			   quickSort(bids, 0, bids.size() - 1);
+
+			   cout << bids.size() << " bids read" << endl;
+
+			   // Calculate elapsed time and display result
+			   ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+			   cout << "time: " << ticks << " clock ticks" << endl;
+			   cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
+
+			   break;
         }
     }
 
